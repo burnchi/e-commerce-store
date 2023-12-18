c3843e06760d7a7857bcec0715b259cc3fc475d5
burnchi <1273732363@qq.com>
2023-12-18 14:54:38 +0800

    显示billboard

---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className=' bg-white border-t'>
+        <div className=' mx-auto py-10'>
+            <p className=' text-center text-xs text-black'>
+                &copy; 2023 Chris
+
+            </p>
+        </div>
+        
+    </footer>
+  )
+}
+
+export default Footer
